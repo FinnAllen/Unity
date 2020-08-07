9f220d7443a0a4061a5cd3bff16bbd41e878cb01
FinnAllen <45641103+FinnAllen@users.noreply.github.com>
2020-08-07 09:10:04 -0700

    Add files via upload
    Keeps NPC Head looking in direction of player

---
+++ NPC_headlook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_headlook : MonoBehaviour
+{
+    public Transform viewTarget;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layerIndex)
+    {
+        if (viewTarget)
+        {
+            anim.SetLookAtPosition(viewTarget.position);
+
+            anim.SetLookAtWeight(2.0f);
+        }
+    }
+}
